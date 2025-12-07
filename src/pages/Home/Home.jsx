b3cffb2741a386b0f21408d26604d3622971bb85
yaswanthkurri <yaswanthkurri02@gmail.com>
2025-12-07 16:27:24 +0530

    Fix import path for AppDownload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import Exploremenu from '../../components/Exploremenu/Exploremenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import Appdownload from '../../components/Appdownload/Appdownload'
+import Appdownload from '../../components/AppDownload/Appdownload'
 const Home = () => {
     const[category,setcategory]=useState("All");
   return (
